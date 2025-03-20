c9dce35ee8968def2968cfbe7a3ed3e0a02bc956
MamaniMarioOrlando <mario_7_89@hotmail.com>
2025-03-20 15:30:25 -0300

    adding logic to the taskModel.js file

---
+++ backend/models/taskModel.js
@@ -0,0 +1,39 @@
+let tasks = [];
+
+const getAllTasks = ()=>{
+    return tasks;
+}
+
+const createTask = (title, descripcion)=>{
+    const newTask = {
+        id: Date.now.toString(),
+        title,
+        description,
+        completed: false,
+        created: new Date()
+    };
+
+    tasks.push(newTask);
+    return newTask;
+};
+
+const updateTask = (id, udpdateData) => {
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    
+    if(taskIndex === -1) return null;
+
+    tasks[taskIndex] = { ...tasks, ...udpdateData};
+    return tasks[taskIndex];
+};
+
+const deleteTask = (id) =>{
+    tasks = tasks.filter(tasks => task.id !== id);
+    return true;
+};
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    updateTask,
+    deleteTask
+}
