fecf4342b301fb451fdfc3604329a21044d1b9e7
MamaniMarioOrlando <mario_7_89@hotmail.com>
2025-03-23 11:04:38 -0300

    Implementing fontend with react+vite

---
+++ frontend/task-frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TaskList } from '../components/TaskList'
+
+export const Home = () => {
+  return (
+    <>
+    <h1>Hola</h1>
+      <TaskList/>
+    </>
+  )
+}
