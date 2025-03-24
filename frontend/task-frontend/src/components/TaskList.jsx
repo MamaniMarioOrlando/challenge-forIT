816df5daf0f5a910d47afb4546f970727dddc094
MamaniMarioOrlando <mario_7_89@hotmail.com>
2025-03-24 19:44:43 -0300

    add components TaskItem, TaskList, Home and configure tailwind.

--- frontend/task-frontend/src/components/TaskList.jsx
+++ frontend/task-frontend/src/components/TaskList.jsx
@@ -17,7 +17,7 @@ export const TaskList = () => {
   return (
     <>
         <div className="space-y-9 max-w-2xl mx-auto mt-10 p-6 bg-white shadow-lg rounded-2xl">
-            <h1 className="text-center text-4xl font-bold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">Lista de tareas</h1>
+            <h1 className="text-center text-4xl font-bold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">List of tasks</h1>
             <div className="space-y-3">
             {
                 tasks.length > 0 ? (
